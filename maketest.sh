d96316360035093ac5c58e7fb8697192ee4e8577
John Schwartzman <john@fortesystem.com>
2019-05-04 17:53:23 -0400

    Add files via upload

--- maketest.sh
+++ maketest.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #############################################################################
 # maketest.sh
-# John Schwartzman, Forte Systems, Inc. 4/26/2019
+# John Schwartzman, Forte Systems, Inc. 05/04/2019
 #
 # A makefile helper script to manage debug and release makefiles 
 # using the same source, object and executable files.
-# In Makefile use:  @source ../maketest.sh && test release debug (for release)
-#						-OR-
-#		    @source ../maketest.sh && test debug release (for debug)
-# Invoke Makefiles with make release, make debug or make clean.
+# In Makefile use:  @source ../maketest.sh && test release debug (release)
+#					@source ../maketest.sh && test debug release (debug)
+# Invoke Makefile with make release, make debug or make clean.
 #
 #############################################################################
 function test()
