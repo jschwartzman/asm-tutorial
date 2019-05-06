603bef874f59eadc403fb1029cddca9cb04dda49
John Schwartzman <john@fortesystem.com>
2019-05-05 22:04:12 -0400

    Add files via upload
    Added assembly code called from C code.

---
+++ prntenv/prntenv.c
@@ -0,0 +1,22 @@
+// prntenv.c
+// assemble prntenv.asm:  yasm -f elf64 -g dwarf2 -o printf.obj printf.asm
+// compile and link:      gcc -g prntenv.c prntenv.obj -o prntenv
+// to execute:            ./prntenv
+
+#include <stdlib.h>     // definition of EXIT_SUCCESS
+#include <time.h>       // declaration of time; definition of time_t
+#include <string.h>     // declaration of strtok
+
+int prntenv(const char* timestr);   // declaration of asm function
+
+int main(void)
+{
+    time_t  now;
+
+    time(&now);
+    char* strTime = strtok(ctime(&now), "\n");  // remove newline from ctime
+    
+    prntenv(strTime);   // call assembly language function with asciiz param
+    return EXIT_SUCCESS;
+}
+   
